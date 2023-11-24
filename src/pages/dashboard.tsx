e4ee938f56c46d3bd6c14fdd7b7fa4a29e03f854
Thiago José de Queiroz <thiagojqueiroz@outlook.com>
2023-11-23 21:17:35 -0300

    FIX: Dashboard component Disabled because of a package is not imported: parseCookies

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,18 @@
-interface IGetServerSideProps {}
+// interface IGetServerSideProps {}
 
-export const getServerSideProps: IGetServerSideProps = async (ctx) => {
-  const { ["nextauth.token"]: token } = parseCookies(ctx)
+// export const getServerSideProps: IGetServerSideProps = async (ctx) => {
+//   const { ["nextauth.token"]: token } = parseCookies(ctx)
 
-  if (!token) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    }
-  }
+//   if (!token) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     }
+//   }
 
-  return {
-    props: {},
-  }
-}
+//   return {
+//     props: {},
+//   }
+// }
