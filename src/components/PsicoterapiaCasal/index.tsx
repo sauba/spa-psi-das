3894e64def862ae8a6b1ef26973dd7b808bdd179
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2023-12-05 21:20:15 -0300

    Atualizar o index.tsx

--- src/components/PsicoterapiaCasal/index.tsx
+++ src/components/PsicoterapiaCasal/index.tsx
@@ -51,27 +51,7 @@ export default function PsicoterapiaCasal() {
         <p
           className={`text-justify px-6 xl:mb-80 pb-8 xl:pb-80 3xl:text-7xl 3xl:pt-80`}
         >
-          A terapia de casal é um processo mediado por profissional
-          especializado, tendo como base o conhecimento científico, os
-          princípios e as técnicas da Psicologia. Para além de um espaço que
-          acolhe demandas afetivas, é também, uma relação profissional entre o
-          terapeuta e o casal. Nesta, o psicólogo auxilia na identificação de
-          necessidades e na compreensão de conflitos, sendo o casal, através de
-          seu entendimento, ações e decisões, o responsável pelo enfrentamento e
-          resolução de suas próprias questões. A terapia de casal promove uma
-          comunicação mais assertiva e reflexiva, bem como o entendimento e
-          alinhamento das expectativas entre o casal, além de ampliar o
-          conhecimento de cada um sobre sua própria personalidade e a do
-          parceiro, contribuindo para uma melhor convivência, compreensão e
-          fortalecimento do compromisso. Esta terapia, também pode auxiliar
-          quando o casal escolhe interromper a relação, contribuindo para que o
-          processo que envolve o término seja mais consciente e saudável,
-          podendo ser também menos doloroso, dentro das possibilidades. As
-          sessões de casal tem duração de 1 hora, podendo acontecer no período
-          quinzenal ou com frequência semanal, conforme a necessidade e análise
-          de cada caso. É fundamental ao desenvolvimento do processo o
-          comprometimento e a assiduidade, de ambos, nas sessões.
-        </p>
+A Terapia de Casal atua acolhendo demandas afetivas e auxiliando na identificação das necessidades do casal, bem como na compreensão e resolução de conflitos. Pra isto, é fundamental o comprometimento das partes, a fim de desenvolver uma comunicação mais assertiva e reflexiva, ampliar o conhecimento de cada um sobre sua personalidade e a do outro, o que pode fortalecer o vínculo e refletir positivamente na convivência.        </p>
       </div>
     </div>
   )
