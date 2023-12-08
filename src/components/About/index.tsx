cdb1b7acbbe6ef89f822d6e3b1644d3865f198ed
Thiago José de Queiroz <thiagojqueiroz@outlook.com>
2023-12-08 12:44:21 -0300

    update: About Component v10

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,7 +12,7 @@ export default function About() {
         min-h-screen
         justify-start
         items-center
-      text-amber-700
+      text-amber-900
       `}
       id="aboutDaniele"
     >
