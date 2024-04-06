0adec783b29b8a697c0950966c3db8debe916d6d
Thiago José de Queiroz <thiagojqueiroz@outlook.com>
2024-04-06 19:49:39 -0300

    Update: About component include youtube iframe v8

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -42,7 +42,7 @@ export default function About() {
       </span>
 
       <div className="w-full">
-        <iframe width="560" height="315" className={`mx-auto rouded-3xl`} src="https://www.youtube.com/embed/e22D0IZ7nkE?si=Qh-KaPvCSXL4P7Gj" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" rel="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="560" height="315" className={`mx-auto rounded-3xl border-none outline-none`} src="https://www.youtube.com/embed/e22D0IZ7nkE?si=Qh-KaPvCSXL4P7Gj" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" rel="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
         {/* <Image
           src={imgProfile}
